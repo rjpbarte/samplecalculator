67dc0ccec067b500da5853762e038a85be5d416b
Kyle Alelis <aleliskyleangelo@yahoo.com>
2019-05-24 14:43:57 +0800

    made Initial code for calculator

--- main.py
+++ main.py
@@ -1,4 +1,42 @@
 import sys
+import os
+
+
+def add(addend, addend1):
+	# Enter addition code here
+	print("")
+
+def sub(first, second):
+	# Enter subtraction code here
+	print("")
+
+def multi(multiplicand, multiplier):
+	# Enter multiplication code here
+	print("")
+
+def div(dividend, divisor):
+	# Enter division code here
+	print("")
 
 if __name__ == "__main__":
-	print("Hello World!")
+	print ("Calculator")
+	print ("A. Addition")
+	print ("B. Subtraction")
+	print ("C. Multiplication")
+	print ("D. Division")
+	print ("Please choose what operation to perform: ")
+	opt = input().upper()
+	os.system('clear')
+	print ("Enter first operand: ")
+	x = int(input())
+	print ("Enter second operand: ")
+	y = int(input())
+	if opt == 'A':
+		add(x,y)
+	elif opt == 'B':
+		sub(x,y)
+	elif opt == 'C':
+		multi(x,y)
+	elif opt == 'D':
+		div(x, y)
+
